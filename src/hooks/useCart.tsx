abac9d42a7048cdb12d1be244f0e4fc9aa75b823
danielcnascimento <ddaniel.nascimento@gmail.com>
2021-05-29 21:52:03 -0300

    added logic function - addProduct

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,20 +23,54 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
-
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    const storagedCart = localStorage.getItem('@RocketShoes:cart');
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      /** -> Instructions
+       * 
+       * 1.Must set items on localStorage
+       * 2.Must Check if Produc Exists
+       * 3.If Product Exist -> update amount, if not -> add the item on Cart
+       * 4.Must check if the desired amount exists on stock, if not,
+       *   return a toast to users saying "cant add more, out of stcok!"
+       * 5.Catch must have toat message "Error on adding product"
+       */
+      let updatedCart = [...cart]
+      const cartExist = cart.find(cartItem => cartItem.id === productId);
+
+      if(cartExist){
+        const stock = await api.get<Stock>(`stock/${productId}`);
+        const stockAmount = stock.data.amount;
+
+        if(cartExist.amount > stockAmount) {
+          toast.error('Quantidade solicitada fora de estoque');
+          return
+        }
+
+        updatedCart = cart.map(cart =>{
+        if(cart.id === productId){
+
+          let updatedAmount = {...cart, amount: cart.amount + 1}
+          return updatedAmount;
+        }
+
+        return cart;
+        })
+      } else {
+        const product = await api.get<Product>(`products/${productId}`);
+        updatedCart.push(product.data)
+      }
+      
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto');
     }
   };
 
